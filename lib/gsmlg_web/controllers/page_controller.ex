c330fd9bb77f476bab56e90b12aebc06449e1aed
gsmlg <gsmlg@gsmlg.com>
2018-05-09 23:05:10 +0800

    init

---
+++ lib/gsmlg_web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule GsmlgWeb.PageController do
+  use GsmlgWeb, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
---
+++ lib/gsmlg_web/views/page_view.ex
@@ -0,0 +1,3 @@
+defmodule GsmlgWeb.PageView do
+  use GsmlgWeb, :view
+end
