8aadb6583fed3c81c9a3fe9a6eff2a5d8c8d5214
gsmlg <gsmlg@gsmlg.com>
2018-05-17 23:06:04 +0800

    add Phoenix socket

---
+++ assets/app/containers/Phoenix/constants.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * Phoenix constants
+ *
+ */
+
+const domain = 'app/Phoenix';
+
+export const INIT = `${domain}/INIT`;
+
+export const MOUNT = `${domain}/MOUNT`;
+
+export const UNMOUNT = `${domain}/UNMOUNT`;
+
+export const SET_SOCKET = `${domain}/SET_SOCKET`;
+
+export const LOG_SOCKET = `${domain}/LOG_SOCKET`;
---
+++ assets/app/containers/Phoenix/tests/selectors.test.js
@@ -0,0 +1,8 @@
+// import { fromJS } from 'immutable';
+// import { selectPhoenixDomain } from '../selectors';
+
+describe('selectPhoenixDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
